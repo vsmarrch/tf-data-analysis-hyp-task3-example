eac00d25e8ca9cf84ef20e0b58ef73290344a28c
Viktoria Marchenko <92745008+vsmarrch@users.noreply.github.com>
2023-04-09 03:50:39 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,11 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 260376781 # Ваш chat ID, не меняйте название переменной
+
+from scipy.stats import mannwhitneyu
 
 def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    p_val = mannwhitneyu(x, y).pvalue
+    answer = False if p_val > 0.01 else True
+    return answer # Ваш ответ, True или False
