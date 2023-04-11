2fa2f74842bcd1518fa13b450c299474e975c40f
Viktoria Marchenko <92745008+vsmarrch@users.noreply.github.com>
2023-04-11 09:13:13 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,9 +4,9 @@ import numpy as np
 
 chat_id = 260376781 # Ваш chat ID, не меняйте название переменной
 
-from scipy.stats import mannwhitneyu
+from scipy.stats import ttest_ind
 
 def solution(x, y) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    p_val = mannwhitneyu(x, y).pvalue
+    p_val = ttest_ind(x, y, equal_var=False).pvalue
     answer = False if p_val > 0.01 else True
     return answer # Ваш ответ, True или False
